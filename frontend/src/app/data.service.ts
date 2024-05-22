bd03e72b8201134fbb9c38dc2e035dd8bae94a00
Maha Marhag <maha.merhag@gmail.com>
2024-05-22 18:52:10 +0200

    project setup

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';  // Import the environment
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post('https://testeraziz-047f20242fc1.herokuapp.com/login', {username, password});
+    return this.http.post(`${this.apiUrl}/login`, {username, password});
   }
 
   getData(): Observable<any> {
-    return this.http.get<any>('https://testeraziz-047f20242fc1.herokuapp.com/data');
+    return this.http.get<any>(`${this.apiUrl}/data`);
   }
 
   updateData(element: any): Observable<any> {
-    return this.http.post('https://testeraziz-047f20242fc1.herokuapp.com/update_data', element);
+    return this.http.post(`${this.apiUrl}/update_data`, element);
   }
 
   addData(newData: any): Observable<any> {
-    return this.http.post(`https://testeraziz-047f20242fc1.herokuapp.com/add_data`, newData);
+    return this.http.post(`${this.apiUrl}/add_data`, newData);
   }
-
 }
-
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://testeraziz-047f20242fc1.herokuapp.com'
+};
